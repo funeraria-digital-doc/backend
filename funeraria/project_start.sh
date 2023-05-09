40385bc6949d515f2dbdec3a98fd93d3e0e5cac1
aluno19262 <aluno19262@ipt.pt>
2023-05-09 19:07:44 +0100

    dockerfile update

--- funeraria/project_start.sh
+++ funeraria/project_start.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
 echo "finished successfully"
-python manage.py runserver 0.0.0.0:9000
+python manage.py runserver 0.0.0.0:8000
