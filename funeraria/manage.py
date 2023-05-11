09b0e35c5f0e7f280fff0c845a14083902a2d265
aluno19262 <aluno19262@ipt.pt>
2023-05-11 09:08:08 +0100

    new

--- funeraria/manage.py
+++ funeraria/manage.py
@@ -9,9 +9,10 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funeraria.settings')
     try:
+        call(["pip", "freeze"])
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        call(["pip", "freeze"])
+        
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
