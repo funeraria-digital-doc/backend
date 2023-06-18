733da3661568af4b023f6e9c0641e05c194dc645
joaorga1 <joaorga1@gmail.com>
2023-06-18 11:27:20 +0100

    fix groups create

--- funeraria/groups/views.py
+++ funeraria/groups/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,7 +11,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from rest_framework.decorators import parser_classes
-
+logger = logging.getLogger(__name__)
 @swagger_auto_schema(
     method='post',
     request_body=GroupCreateSerializer,
@@ -25,7 +26,10 @@ def create(request, *args, **kwargs):
         if serializer.is_valid():
             try:
                 serializer.save()
-                group['group']  = serializer.data
+                group['group']  = dict({
+                    'id' : serializer.instance.id,
+                    'name' : serializer.instance.name
+                })
                 group['msg']  = "Group created successfully"
             except Exception as e:
                 group['error']   = serializer.errors
