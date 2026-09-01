4bb87a180e5fae9cbe845746529a0c6a4df671f0
João Alves <joaorga1@gmail.com>
2026-09-01 08:14:35 +0100

    Move hardcoded secrets (SECRET_KEY, email, mongo) to environment variables

--- funeraria/funeraria/settings.py
+++ funeraria/funeraria/settings.py
@@ -27,10 +27,10 @@ DatabaseOperations.conditional_expression_supported_in_where_clause = (
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED-DJANGO-SECRET-KEY***'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
@@ -46,10 +46,10 @@ CORS_ALLOW_CREDENTIALS = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = '118ee460309c78'
-EMAIL_HOST_PASSWORD = "***REMOVED-MAILTRAP-PASSWORD***"
-EMAIL_PORT = "2525"
+EMAIL_HOST = config('EMAIL_HOST', default='sandbox.smtp.mailtrap.io')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', default='2525')
 EMAIL_USE_TLS = True
 
 # Application definition
@@ -125,14 +125,7 @@ DATABASES = {
         'ENFORCE_SCHEMA': True,
         'CONN_MAX_AGE': 3600*24,
         'CLIENT': {
-            # 'host': 'mongodb-dev',
-            # 'port' : 27017,
-            # 'password' : '***REMOVED-MONGO-PASSWORD***',
-            # 'username' : 'funerariadigitaldoc',
-            # 'authSource': 'admin',
-            # 'authMechanism' : 'SCRAM-SHA-1'
-            #'host':  'mongodb+srv://funerariadigitaldoc:***REMOVED-MONGO-PASSWORD***@funerariamongo.jbar8li.mongodb.net/?retryWrites=true&w=majority'
-            'host': 'mongodb://funeraria:***REMOVED-MONGO-PASSWORD***@192.168.1.26:27017/admin?retryWrites=true&loadBalanced=false&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1',
+            'host': config('MONGO_HOST'),
         }
     }
 }
